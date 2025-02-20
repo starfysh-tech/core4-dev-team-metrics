0c982801992adeb2118236fbb72851151990ce1a
Randall Noval <randall.noval@teladochealth.com>
2025-02-20 18:02:43 -0500

    adding sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
@@ -9,10 +10,19 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "randall-noval",
+      project: "core4-dev-team-metrics",
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
